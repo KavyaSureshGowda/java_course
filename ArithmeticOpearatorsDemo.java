f0af7f834114dad2b3fc3db423a68744e8409795
KavyaSureshGowda <56511241+KavyaSureshGowda@users.noreply.github.com>
2021-09-26 20:02:21 +0530

    Add files via upload

---
+++ ArithmeticOpearatorsDemo.java
@@ -0,0 +1,15 @@
+class ArithmeticOpearatorsDemo{
+public static void main(String[] args){
+int a=20;
+int b=10;
+int addResult=a+b;
+System.out.println("addResult:"+addResult);
+int c=a-b;
+System.out.println("c:"+c);
+int d=a*b;
+System.out.println("d:"+d);
+int e=a/b;
+System.out.println("e:"+e);
+}
+}
+
